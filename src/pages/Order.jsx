08f41056ef5e58253564b1e0a73c2100aab167d5
Soumya Manna <mannasoumya009@gmail.com>
2024-05-19 12:11:04 +0530

    last 5 years setting

--- src/pages/Order.jsx
+++ src/pages/Order.jsx
@@ -1,24 +1,52 @@
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import Authlayout from './layouts/Authlayout';
 import { useParams } from 'react-router-dom';
 import Cookies from 'js-cookie';
+import { titleContext } from '../contextApis/TitleContext';
 
 function Order() {
     const params = useParams();
     const [year, setyear] = useState(new Date().getFullYear());
+    const [orderDetails, setorderDetails] = useState({});
+    const [lastYears, setlastYears] = useState([]);
+    const title = useContext(titleContext);
+
     const fetchOrderDetails = () => {
         fetch(`${process.env.REACT_APP_SERVER_URL}/get-customer-order-data/${params.customer_id}/${year}`, {
             headers: {
                 'Authorization': Cookies.get('secret_token')
             }
         })
-            .then(response => {
-                let data = response.json();
-                console.log(data);
+            .then(async response => {
+                let data = await response.json();
+                let object = {
+                    "customer_name": data.data[0].name,
+                    "phone": data.data[0].phone,
+                    "address": data.data[0].address,
+                    "email": data.data[0].email,
+                    "order_data": data.data,
+                }
+                setorderDetails(object);
             })
             .catch(error => console.log(error));
     }
+
+    const getYearArray = () => {
+        let yearsList = [];
+        let year = new Date().getFullYear();
+        yearsList.push(year);
+        for (let index = 1; index <= 5; index++) {
+            yearsList.push(year - index);
+        }
+        setlastYears(yearsList);
+    }
+    const handleYearChange = () => {
+
+    }
+
     useEffect(() => {
+        title.settitle("Customer Order");
+        getYearArray();
         fetchOrderDetails();
     }, []);
     return (
@@ -27,8 +55,18 @@ function Order() {
                 <div className="row g-2 align-items-center">
                     <div className="col-10">
                         <h2 className="page-title m-2">
-                            Orders of
+                            Orders of {orderDetails.customer_name}
                         </h2>
+                    </div>
+                    <div className="col-2">
+                        <div className="form-group">
+                            <label htmlFor="" className="form-label">Year</label>
+                            <select name="" id="" className="form-control" value={year} onChange={() => { handleYearChange() }}>
+                                {lastYears && lastYears.map((element, index) => (
+                                    <option value={element} key={index}>{element}</option>
+                                ))}
+                            </select>
+                        </div>
                     </div>
                     <div className="col-12">
                         <div className="card">
@@ -36,79 +74,19 @@ function Order() {
                                 <table className="table table-vcenter card-table table-striped">
                                     <thead>
                                         <tr>
-                                            <th>Name</th>
-                                            <th>Title</th>
-                                            <th>Email</th>
-                                            <th>Role</th>
-                                            <th className="w-1"></th>
+                                            <th>product Name</th>
+                                            <th>Price</th>
+                                            <th>Date of purchase</th>
                                         </tr>
                                     </thead>
                                     <tbody>
-                                        <tr>
-                                            <td>Maryjo Lebarree</td>
-                                            <td className="text-secondary">
-                                                Civil Engineer, Product Management
-                                            </td>
-                                            <td className="text-secondary"><a href="#" className="text-reset">mlebarree5@unc.edu</a></td>
-                                            <td className="text-secondary">
-                                                User
-                                            </td>
-                                            <td>
-                                                <a href="#">Edit</a>
-                                            </td>
-                                        </tr>
-                                        <tr>
-                                            <td>Egan Poetz</td>
-                                            <td className="text-secondary">
-                                                Research Nurse, Engineering
-                                            </td>
-                                            <td className="text-secondary"><a href="#" className="text-reset">epoetz6@free.fr</a></td>
-                                            <td className="text-secondary">
-                                                Admin
-                                            </td>
-                                            <td>
-                                                <a href="#">Edit</a>
-                                            </td>
-                                        </tr>
-                                        <tr>
-                                            <td>Kellie Skingley</td>
-                                            <td className="text-secondary">
-                                                Teacher, Services
-                                            </td>
-                                            <td className="text-secondary"><a href="#" className="text-reset">kskingley7@columbia.edu</a></td>
-                                            <td className="text-secondary">
-                                                User
-                                            </td>
-                                            <td>
-                                                <a href="#">Edit</a>
-                                            </td>
-                                        </tr>
-                                        <tr>
-                                            <td>Christabel Charlwood</td>
-                                            <td className="text-secondary">
-                                                Tax Accountant, Engineering
-                                            </td>
-                                            <td className="text-secondary"><a href="#" className="text-reset">ccharlwood8@nifty.com</a></td>
-                                            <td className="text-secondary">
-                                                Owner
-                                            </td>
-                                            <td>
-                                                <a href="#">Edit</a>
-                                            </td>
-                                        </tr>
-                                        <tr>
-                                            <td>Haskel Shelper</td>
-                                            <td className="text-secondary">
-                                                Staff Scientist, Legal
-                                            </td>
-                                            <td className="text-secondary"><a href="#" className="text-reset">hshelper9@woothemes.com</a></td>
-                                            <td className="text-secondary">
-                                                Admin
-                                            </td>
-                                            <td>
-                                                <a href="#">Edit</a>
-                                            </td>
-                                        </tr>
+                                        {orderDetails.order_data && orderDetails.order_data.map((element, index) => (
+                                            <tr key={index}>
+                                                <td>{element.product_name}</td>
+                                                <td>{element.price}</td>
+                                                <td>{element.date}</td>
+                                            </tr>
+                                        ))}
                                     </tbody>
                                 </table>
                             </div>
